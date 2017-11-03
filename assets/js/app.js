e45299b3e5d25f32b222a943e0454231e4ebf5f8
Marcial Soto <marcialsotosanchez@gmail.com>
2017-11-02 22:28:48 -0500

    Mejorado el gulpfile

---
+++ assets/js/app.js
@@ -0,0 +1,2 @@
+//import '../node_modules/bootstrap/dist/js/bootstrap';
+console.log('Hola');
--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,37 @@
 var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
+var concat        = require('gulp-concat');
+var rename        = require('gulp-rename');
 var sass        = require('gulp-sass');
+var prefix        = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
+gulp.task('serve', function() {
 
     browserSync.init({
         server: "./"
     });
 
-    gulp.watch("scss/*.scss", ['sass']);
+    gulp.watch("assets/scss/*.scss", ['styles']);
+    gulp.watch("assets/js/*.js", ['scripts']);
     gulp.watch("*.html").on('change', browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
-    return gulp.src("scss/*.scss")
+gulp.task('styles', function() {
+    return gulp.src("assets/scss/*.scss")
         .pipe(sass())
+        .pipe(prefix(['last 15 versions'], { cascade: true }))
         .pipe(gulp.dest("dist/css"))
+        .pipe(browserSync.stream());
+});
+
+// Compile js into dist folder
+gulp.task('scripts', function() {
+    return gulp.src("assets/js/*.js")
+        .pipe(concat('concat.js'))
+        .pipe(rename('app.js'))
+        .pipe(gulp.dest("dist/js"))
         .pipe(browserSync.stream());
 });
 
